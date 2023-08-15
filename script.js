51a3836e5eba25cbebb12eb55249e29ba2c0ad36
daniash84 <danielle.hess84@gmail.com>
2023-08-14 18:26:53 -0700

    trouble shooting

--- script.js
+++ script.js
@@ -1,16 +1,13 @@
 const fs = require('fs')
 const inquirer = require('inquirer');
 
-const generateMarkdown = require('./utils/generateMarkdown.js');
-const api = require('./utils/api.js');
-
 const questions = [
    
     {
     type: 'input',
     message: 'What is your GitHub username?',
     name: 'github',
-}
+},
 {
     type: 'input',
     message: 'What is the name of your GitHub Repo?',
@@ -56,7 +53,7 @@ const questions = [
         message: "Choose a license for your project.",
         choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
         name: 'license'
-    }
+    },
     ];
     function writeToFile(fileName, data) {
         fs.writeFile(fileName, data, err => {
@@ -66,30 +63,31 @@ const questions = [
             console.log("Success!")
         });
     }
-    const writeFileAsync = util.promisify(writeToFile);
-    async function init() {
-        try{
+    function init() {
+        inquirer
+            .prompt(questions)
+    }
+        // try{
             
-            const userResponse = await inquirer.prompt(questions);
-            console.log('Your Response: ', userResponse);
-            console.log("Retrieving your GitHub data next");
+        //     const userResponse = inquirer.prompt(questions);
+        //     console.log('Your Response: ', userResponse);
+        //     console.log("Retrieving your GitHub data next");
     
-            const userInfo = await api.getUser(userResponse);
-            console.log("GitHub user info: ", userInfo);
-            console.log("Building your beautiful README");
+        //     const userInfo = api.getUser(userResponse);
+        //     console.log("GitHub user info: ", userInfo);
+        //     console.log("Building your beautiful README");
     
-            const markdown = generateMarkdown(userResponse, userInfo);
-            console.log(markdown);
+        //     const markdown = generateMarkdown(userResponse, userInfo);
+        //     console.log(markdown);
     
-            await writeFileAsync('ExampleREADME.md', markdown);
-            console.log("README is Completed....Now you can relax");
+        //     await writeFileAsync('ExampleREADME.md', markdown);
+        //     console.log("README is Completed....Now you can relax");
     
         
-        } catch  (error) {
-            console.error();
-        }
+        // } catch  (error) {
+        //     console.error();
+        // }
     
-    }
     
     
     init();
